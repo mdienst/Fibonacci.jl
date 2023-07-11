440ad8ab93224714dafd4c06f93baa7e5aaba32d
Manuel Dienst <manuel.dienst@student.uni-siegen.de>
2023-07-11 13:27:00 +0200

    added test loop

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,17 +1,16 @@
 using Fibonacci
 using Test
 
-n = [0, 1, 30]
-f = [0, 1, 832040]
 
-@test fibonacci(0)[1] == 0
-@test fibonacci(1)[1] == 1
-@test fibonacci(30)[1] == 832040
 
 #for i = 1:length(n)
 #    @test fibonacci(n[i])[1] == f[i]
 #end
 
-#@testset "Fibonacci.jl" begin
-    # Write your tests here.
-#end
+@testset "Fibonacci.jl" begin
+    res = [(0,0),(1,1),(30,832040)]
+    for (i,o) in res
+        @test fibonacci(i)[1] == o
+    end
+# Write your tests here.
+end
