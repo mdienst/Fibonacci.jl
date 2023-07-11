f04c870c3400321b909d1f8cddc8b9a51eb983ba
Manuel Dienst <manuel.dienst@student.uni-siegen.de>
2023-07-11 12:49:54 +0200

    Added 3 tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,17 @@
 using Fibonacci
 using Test
 
-@testset "Fibonacci.jl" begin
+n = [0, 1, 30]
+f = [0, 1, 832040]
+
+@test fibonacci(0)[1] == 0
+@test fibonacci(1)[1] == 1
+@test fibonacci(30)[1] == 832040
+
+#for i = 1:length(n)
+#    @test fibonacci(n[i])[1] == f[i]
+#end
+
+#@testset "Fibonacci.jl" begin
     # Write your tests here.
-end
+#end
