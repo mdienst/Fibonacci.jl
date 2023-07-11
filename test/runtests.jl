5771a5b6a1958ac91855b2e0e05c41e0e2c8d0bb
Manuel Dienst <manuel.dienst@student.uni-siegen.de>
2023-07-11 13:58:11 +0200

    deleted lines

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,16 +1,9 @@
 using Fibonacci
 using Test
 
-
-
-#for i = 1:length(n)
-#    @test fibonacci(n[i])[1] == f[i]
-#end
-
 @testset "Fibonacci.jl" begin
     res = [(0,0),(1,1),(30,832040)]
     for (i,o) in res
         @test fibonacci(i)[1] == o
     end
-# Write your tests here.
 end
